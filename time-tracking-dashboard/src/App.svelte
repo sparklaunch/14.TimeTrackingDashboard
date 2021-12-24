fb200c66381c40dae999d3cbf0d4b787c56569e0
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-24 10:58:23 +0900

    Add the profile card

--- time-tracking-dashboard/src/App.svelte
+++ time-tracking-dashboard/src/App.svelte
@@ -1,1 +1,22 @@
-<div id="app" />
+<script lang="ts">
+    import Profile from "./components/Profile.svelte";
+</script>
+
+<div id="app">
+    <div id="container">
+        <Profile />
+    </div>
+</div>
+
+<style>
+    #app {
+        display: flex;
+        height: 100%;
+        justify-content: center;
+        align-items: center;
+    }
+    #container {
+        display: grid;
+        grid-template-areas: "profile work play study" "profile exercise social self-care";
+    }
+</style>
---
+++ time-tracking-dashboard/src/components/Profile.svelte
@@ -0,0 +1,59 @@
+<div id="profile">
+    <div id="title">
+        <img src="/assets/image-jeremy.png" alt="Jeremy" />
+        <p>Report for</p>
+        <h1>Jeremy Robson</h1>
+    </div>
+    <ul>
+        <li>Daily</li>
+        <li class="active">Weekly</li>
+        <li>Monthly</li>
+    </ul>
+</div>
+
+<style>
+    #profile {
+        grid-area: profile;
+    }
+    #title {
+        display: flex;
+        flex-direction: column;
+        background-color: rgb(77, 62, 231);
+        padding: 30px;
+        border-radius: 10px;
+        margin: -30px -30px 0 -30px;
+    }
+    #title > img {
+        border: 3px solid white;
+        border-radius: 50%;
+        width: 80px;
+        margin-bottom: 30px;
+    }
+    #title > p {
+        color: white;
+        opacity: 0.5;
+        font-size: 14px;
+    }
+    #title > h1 {
+        color: white;
+        font-weight: 300;
+        font-size: 36px;
+        margin-bottom: 50px;
+    }
+    #profile {
+        background-color: rgb(27, 29, 66);
+        padding: 30px;
+
+        border-radius: 10px;
+    }
+    #profile > ul {
+        list-style: none;
+    }
+    #profile > ul > li {
+        color: rgb(99, 103, 167);
+        margin-top: 20px;
+    }
+    .active {
+        color: white !important;
+    }
+</style>
