bf6aac5fdca272356661a39665a7ac59679699ea
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-24 12:26:02 +0900

    Add the item interface

--- time-tracking-dashboard/src/App.svelte
+++ time-tracking-dashboard/src/App.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import Profile from "./components/Profile.svelte";
+    type TimeMarker = {
+        current: number;
+        previous: number;
+    };
+    interface TimeFrame {
+        daily: TimeMarker;
+        weekly: TimeMarker;
+        monthly: TimeMarker;
+    }
+    interface Item {
+        title: string;
+        timeframes: TimeFrame;
+    }
+    onMount(async () => {
+        const response: Response = await fetch("/data.json");
+        const data: Item[] = await response.json();
+    });
 </script>
 
 <div id="app">
